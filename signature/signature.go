f47872cdfc01ed4c52aaffe24777fe5f33119678
Yusuf <yusufozmis2@gmail.com>
2026-05-05 14:15:54 +0300

    feat: add public key and signature helpers for hex decoding

--- signature/signature.go
+++ signature/signature.go
@@ -17,6 +17,19 @@ func (sig *Signature) String() string {
 	return hex.EncodeToString(sig.value)
 }
 
+// input sig string is expected to be hex encoded
+func DecodeSignatureFromString(sig string) (*Signature, error) {
+
+	sigValue, err := hex.DecodeString(sig)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Signature{
+		value: sigValue,
+	}, nil
+}
+
 func NewSignatureFromBytes(sig []byte) *Signature {
 	return &Signature{
 		value: sig,
