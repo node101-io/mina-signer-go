87541abba07e0fee0a528cc610b32f9209b581fa
Yusuf <yusufozmis2@gmail.com>
2026-05-14 16:01:46 +0300

    feat: implement merkle lists

--- errors/errors.go
+++ errors/errors.go
@@ -3,6 +3,8 @@ package errors
 import "errors"
 
 var ErrNilSignature = errors.New("nil signature")
+var ErrInvalidSignatureLenght error = errors.New("invalid signature lenght")
+
 var ErrNilPublicKey = errors.New("nil public key")
 
 var ErrNilPrivateKey = errors.New("nil private key")
